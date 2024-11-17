279cb4ccfcf8488912decb5d8f3d5569f405509c
Tiago <tiago@trattecinformatica.com.br>
2024-11-17 18:15:38 -0300

    Negativo de uma imagem

--- exercicio_2.py
+++ exercicio_2.py
@@ -9,3 +9,12 @@ plt.hist(imagem.ravel(), bins = 256, range = [0,256])
 plt.show()
 
 print(imagem.shape)
+
+# Negativo da imagem
+for i in range(786):
+    for j in range(762):
+        imagem[i,j]=255-imagem[i,j]
+cv2.imwrite('imagem2.png', imagem)
+imagem2 = cv2.imread('imagem2.png')
+cv2.imshow('Imagem Negativo', imagem)
+cv2.waitKey(2000)
