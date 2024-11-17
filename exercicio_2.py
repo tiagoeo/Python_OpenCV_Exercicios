820fd9e750b36cc9db3c30b6df188c42f1abca93
Tiago <tiago@trattecinformatica.com.br>
2024-11-17 18:14:00 -0300

    Histograma de pixels de imagem colorida

--- exercicio_2.py
+++ exercicio_2.py
@@ -7,3 +7,5 @@ imagem = cv2.cvtColor(imagem, cv2.COLOR_BGR2RGB)
 # Exibir histograma de pixels imagem colorida
 plt.hist(imagem.ravel(), bins = 256, range = [0,256])
 plt.show()
+
+print(imagem.shape)
