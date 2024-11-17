9b8cee88ccb56897325cf6c25dff27959050dbf5
Tiago <tiago@trattecinformatica.com.br>
2024-11-17 18:02:47 -0300

    Tranformar nova imagem em RGB

--- exercicio_1.py
+++ exercicio_1.py
@@ -16,3 +16,14 @@ print("Largura (w) %d pixels" % (imagem.shape[1]))
 print("Altura (h) %d pixels" % (imagem.shape[0]))
 print("Canais %d " % (imagem.shape[2]))
 print("Tipo: ", imagem.dtype)
+
+# Salvar nova imagem
+cv2.imwrite("nova_imagem.png", imagem)
+
+# Transformar nova imagem em RGB
+imagem2 = cv2.imread("nova_imagem.png")
+imagem2 = cv2.cvtColor(imagem2, cv2.COLOR_BGR2RGB)
+
+# Mostrando imagem
+cv2.imshow('Nova imagem em RGB', imagem2)
+cv2.waitKey(2000)
